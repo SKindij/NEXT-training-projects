43efecc173339e4390bbd6fbd33bad38f0166fdf
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-02 22:24:06 +0200

    init: add an additional file
    add Footer.tsx

---
+++ blog-app/components/Footer.tsx
@@ -0,0 +1,11 @@
+// components/Footer.tsx
+
+const Footer = () => {
+    return (
+      <footer className="container">
+        <p>Created by &copy;SKindij</p>
+      </footer>
+    )
+}
+
+export {Footer};
