aedd67526e8d1307d8ad923b3309b1d61c13e3f2
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-02 23:39:51 +0200

    init: set up project structure
    create page.tsx

---
+++ blog-app/app/about/team/page.tsx
@@ -0,0 +1,11 @@
+// app/about/team/page.tsx
+export default function Team() {
+  return (
+    <section>
+      <h1>Team page</h1>
+      <p>Robot Werter</p>
+      <p>Barbarella Space</p>
+    </section>
+
+    );
+}
