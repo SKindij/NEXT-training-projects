930468361752f615670f616124816e3e4fb68f79
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-02 22:23:25 +0200

    feat: create new data visualization component
    create Header.tsx

---
+++ blog-app/components/Header.tsx
@@ -0,0 +1,14 @@
+// components/Header.tsx
+import Link from "next/link";
+
+const Header = () => {
+    return (
+      <header className="container">
+        <Link href='/'>Home</Link>
+        <Link href='/blog'>Blog</Link>
+        <Link href='/about'>About</Link>
+      </header>
+    )
+}
+
+export {Header};
