7f4af14c4f4dd6b11db9e84c43e0290d2706ec52
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-23 12:07:00 +0200

    feat: create new data visualization component
    update nav-links.tsx

--- financial-dashboard/app/ui/dashboard/nav-links.tsx
+++ financial-dashboard/app/ui/dashboard/nav-links.tsx
@@ -9,6 +9,8 @@ import {
 import Link from 'next/link';
 // хук для показу активної сторінки
 import { usePathname } from 'next/navigation';
+// бібліотека для стилізації зміни сторінок
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -27,6 +29,7 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
   
   return (
     <>
@@ -36,7 +39,12 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+			  "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+			  {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+			)}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
