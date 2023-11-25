c9abdcc30bf38c4d9414f9d98b87db1e202f073c
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-25 12:37:11 +0200

    refactor: improve code organization and structure
    update page.tsx

--- blog-app/app/blog/page.tsx
+++ blog-app/app/blog/page.tsx
@@ -1,38 +1,18 @@
-// app/blog/page.tsx
+// @file: app/blog/page.tsx
+import { PostSearch } from "@/components/PostSearch";
+import { Posts } from "@/components/Posts";
 import { Metadata } from "next";
-import Link from "next/link";
 
-async function getData() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    next: {
-      revalidate: 60, // дані будуть кешовані на 60 секунд
-    },
-  });
-  // обробка помилки, якщо запит на сервер не вдалось виконати
-    if (!response.ok) throw new Error("Unable to fetch posts!");
-  // розпакування JSON-відповіді в об'єкт
-    return response.json();
-}
+export const metadata: Metadata = {
+  title: "Blog | Next App",
+};
 
-// визначення метаданих сторінки
-  export const metadata:Metadata = {
-    title: "Blog | Next App",
-  };
-
-export default async function Blog() {
-  // викликаємо для отримання списку постів
-  const posts = await getData();
-
+export default function Blog() {
   return (
     <>
       <h1>Blog page</h1>
-      <ul>
-        {posts.map((post:any) => (
-          <li key={post.id}>
-            <Link href={`/blog/${post.id}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <PostSearch />
+      <Posts />
     </>
   );
 }
