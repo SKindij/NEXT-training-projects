fc731d23ffd1714b13c2ea72bc4a044d4835a787
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-25 14:03:53 +0200

    chore: refactor code to maintain code style consistency
    update page.tsx

--- blog-app/app/page.tsx
+++ blog-app/app/page.tsx
@@ -1,10 +1,19 @@
-// app/page.tsx
-
-
+// @file: app/page.tsx
+// це головна (базова) сторінка сайту
 export default function Home() {
   return (
    <>
      <h1>Welcome to Next Blog App</h1>
+     <p>
+        We use:
+      </p>
+      <ul>
+        <li>data from JSON Placeholder Site</li>
+        <li>data obtained from our API</li>
+      </ul>
+      <p>
+        to emulate working with the API.
+      </p>
    </>
   )
 }
