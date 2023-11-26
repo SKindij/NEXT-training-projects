6ecf28158cd059241f046e562e593b36babe92b6
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-26 13:26:28 +0200

    chore: refactor code to maintain code style consistency
    update layout.tsx

--- blog-app/app/layout.tsx
+++ blog-app/app/layout.tsx
@@ -1,31 +1,33 @@
-// app/layout.tsx
+// @file: app/layout.tsx
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-  const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
+import { Providers } from "../components/Providers";
 
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 
-export const metadata: Metadata = {
+export const metadata:Metadata = {
   title: 'Next Blog App',
   description: 'Blog App by create next app',
   
 };
 
-export default function RootLayout({
-  children,
-}: {
+// створюємо HTML розмітку для сайту
+export default function RootLayout({children}: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Providers>
         <Header />
         <main className='container'>
           {children}
         </main>
         <Footer />
+      </Providers>
       </body>
     </html>
   )
