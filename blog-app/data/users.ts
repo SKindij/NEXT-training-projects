2a61878547420ff31f714d9698bc305ac856b285
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-26 13:09:02 +0200

    chore: add new entries to the database
    add "/data/users.ts"

---
+++ blog-app/data/users.ts
@@ -0,0 +1,24 @@
+// @file: data/users.ts
+export const users = [
+    {
+      id: "101",
+      email: "robot@gmail.com",
+      name: "Robot",
+      password: "werter1985",
+      role: "admin",
+    },
+    {
+      id: "102",
+      email: "barbarella@gmail.com",
+      name: "Barbarella",
+      password: "space1968",
+      role: "guest",
+    },
+    {
+      id: "103",
+      email: "tracker@gmail.com",
+      name: "Tracker",
+      password: "busik1999",
+      role: "guest",
+    },
+  ];
