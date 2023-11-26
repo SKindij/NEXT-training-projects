c16784546436377e57795f199ab9dcf2942e70bc
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-26 17:13:56 +0200

    chore: add new entries to the database
    update posts.ts

--- blog-app/app/api/posts/posts.ts
+++ blog-app/app/api/posts/posts.ts
@@ -1,28 +1,54 @@
 // @file: /api/posts/posts.ts
 export const posts = [
   {
-    id: 101,
+    id: 95,
     title: "Napoli won Seria A",
     body: "Some info about italian football",
   },
   {
-    id: 102,
+    id: 96,
     title: "Manchester City won Premiership",
     body: "Some info about English football",
   },
   {
-    id: 103,
+    id: 97,
     title: "Barcelona won La Liga",
     body: "Some info about Spanish football",
   },
   {
-    id: 104,
+    id: 98,
     title: "Bayern Munich triumphs in Bundesliga",
     body: "Some info about German football",
   },
   {
-    id: 105,
+    id: 99,
     title: "PSG clinches Ligue 1 title",
     body: "Some info about French football",
   },
+  {
+    id: 71,
+    title: "Juventus crowned Serie A champions",
+    body: "Some info about Italian football",
+  },
+  {
+    id: 72,
+    title: "Liverpool secures Premier League victory",
+    body: "Some info about English football",
+  },
+  {
+    id: 73,
+    title: "Real Madrid claims La Liga championship",
+    body: "Some info about Spanish football",
+  },
+  {
+    id: 74,
+    title: "RB Leipzig dominates Bundesliga",
+    body: "Some info about German football",
+  },
+  {
+    id: 75,
+    title: "Monaco emerges as Ligue 1 winners",
+    body: "Some info about French football",
+  },
+
 ];
