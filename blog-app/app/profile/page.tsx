0e0117ed1cc134ea702d1e4adb8952ebb2237e2b
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-26 17:15:36 +0200

    chore: perform general project maintenance tasks
    update page.tsx

--- blog-app/app/profile/page.tsx
+++ blog-app/app/profile/page.tsx
@@ -3,6 +3,8 @@
 import { authConfig } from "../../configs/auth";
 // для отримання сесії на сервері
 import { getServerSession } from "next-auth/next";
+// автоматично оптимізує зображення для швидкості завантаження
+import Image from 'next/image';
 
 // відображає профіль користувача
 export default async function Profile() {
@@ -14,7 +16,9 @@ export default async function Profile() {
       {/* відображення імені користувача */}
       <h1>Profile of {session?.user?.name}</h1>
       {/* відображення зображення користувача */}
-      {session?.user?.image && <img src={session.user.image} alt="user img" />}
+      {session?.user?.image && (
+        <Image src={session.user.image} alt="" width={300} height={300} />
+      )}
     </div>
   );
 }
