860b740f179c713c1511f47d9fb48ab183904959
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-28 17:06:24 +0200

    perf: make changes to improve overall project performance
    update "@/app/ui/invoices/buttons"

--- financial-dashboard/app/ui/invoices/buttons.tsx
+++ financial-dashboard/app/ui/invoices/buttons.tsx
@@ -1,3 +1,4 @@
+// @file: @/app/ui/invoices/buttons
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
@@ -13,10 +14,10 @@ export function CreateInvoice() {
   );
 }
 
-export function UpdateInvoice({ id }: { id: string }) {
+export function UpdateInvoice({ id }: { id:string }) {
   return (
     <Link
-      href="/dashboard/invoices"
+      href={`/dashboard/invoices/${id}/edit`}
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
@@ -24,7 +25,7 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
-export function DeleteInvoice({ id }: { id: string }) {
+export function DeleteInvoice({ id }: { id:string }) {
   return (
     <>
       <button className="rounded-md border p-2 hover:bg-gray-100">
