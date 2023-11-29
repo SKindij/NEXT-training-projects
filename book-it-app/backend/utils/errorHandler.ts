680678a53dc51272f0761eced0343b5dbe7a0c1f
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-29 21:42:25 +0200

    init: create an additional file
    add "/utils/errorHandler.ts"

---
+++ book-it-app/backend/utils/errorHandler.ts
@@ -0,0 +1,11 @@
+// @path: @/backend/utils/errorHandler.ts
+class ErrorHandler extends Error {
+  statusCode: number;
+
+  constructor(errMessage: string, statusCode: number) {
+    super(errMessage);
+    this.statusCode = statusCode;
+  }
+}
+
+export default ErrorHandler;
