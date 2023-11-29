a829cc99934108cccbba777ec9c4491e5c52b81e
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-29 14:31:32 +0200

    feat: add entirely new capabilities
    update actions.ts

--- financial-dashboard/app/lib/actions.ts
+++ financial-dashboard/app/lib/actions.ts
@@ -45,10 +45,32 @@ export async function createInvoice(formData:FormData) {
   redirect('/dashboard/invoices');
 }
 
+// визначення схеми для оновлення рахунку, виключаючи id і date
+const UpdateInvoice = FormSchema.omit({ id:true, date:true });
+
+export async function updateInvoice(id:string, formData:FormData) {
+  // витягуємо необхідні дані з форми та валідуємо їх
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  // переводимо суму рахунку з доларів в центи
+  const amountInCents = amount * 100;
+  // використовуємо sql-запит для оновлення в базу даних
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+  // для перевалідації шляху та перенаправлення на потрібну сторінку
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
 
 
-
-
-
-
-
+export async function deleteInvoice(id:string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  // ініціює новий запит на сервер і повторно відтворить таблицю
+  revalidatePath('/dashboard/invoices');
+}
