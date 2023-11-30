a4040a5c5187eff930420780349531bdbacadbc3
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-30 21:48:19 +0200

    docs: clarify descriptions of key functionalities
    update "/app/api/rooms/[id]/route.ts"

--- book-it-app/app/api/rooms/[id]/route.ts
+++ book-it-app/app/api/rooms/[id]/route.ts
@@ -1,24 +1,24 @@
 // @path: @/app/api/rooms/[id]/route.ts
 import dbConnect from "@/backend/config/dbConnect";
-import {
-  getRoomDetails,
-  updateRoom,
-} from "@/backend/controllers/roomControllers";
+import { getRoomDetails, updateRoom } from "@/backend/controllers/roomControllers";
+// функціонал створення маршруту
 import { createEdgeRouter } from "next-connect";
 import { NextRequest } from "next/server";
-
+// типи для визначення контексту
 interface RequestContext {
   params: {
-    id: string;
+    id:string;
   };
 }
 
+// створюємо роутер через next-connect для обробки запитів
 const router = createEdgeRouter<NextRequest, RequestContext>();
-
+// встановлюємо з'єднання з базою даних перед обробкою запитів
 dbConnect();
-
+// налаштовуємо обробку запиту для отримання деталей кімнати за її ідентифікатором
 router.get(getRoomDetails);
 
+// функція, яка буде обробляти GET-запити до цього маршруту
 export async function GET(request: NextRequest, ctx: RequestContext) {
   return router.run(request, ctx);
 }
