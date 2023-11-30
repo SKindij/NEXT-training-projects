882226506801cf0d656e832193d14ab650d37852
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-30 21:22:37 +0200

    docs: clarify descriptions of key functionalities
    update "/app/api/admin/rooms/route.ts"

--- book-it-app/app/api/admin/rooms/route.ts
+++ book-it-app/app/api/admin/rooms/route.ts
@@ -1,17 +1,20 @@
 // @path: @/app/api/admin/rooms/route.ts
+// функція з'єднання з базою даних
 import dbConnect from "@/backend/config/dbConnect";
+// функція створення нової кімнати
 import { newRoom } from "@/backend/controllers/roomControllers";
+// функція створення маршрутизатора
 import { createEdgeRouter } from "next-connect";
 import { NextRequest } from "next/server";
-
+// Інтерфейс для контексту запиту
 interface RequestContext {}
-
+// створення маршрутизатора
 const router = createEdgeRouter<NextRequest, RequestContext>();
-
+// виклик функції з'єднання з базою даних
 dbConnect();
-
+// налаштування маршруту POST для створення нової кімнати
 router.post(newRoom);
-
-export async function POST(request: NextRequest, ctx: RequestContext) {
+// виклик маршрутизатора для обробки запиту
+export async function POST(request:NextRequest, ctx:RequestContext) {
   return router.run(request, ctx);
 }
