380252e6c7569885f647ca8e3cd124f8eb1fcc96
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-11-30 21:31:16 +0200

    docs: clarify descriptions of key functionalities
    update "/app/api/admin/rooms/[id]/route.ts"

--- book-it-app/app/api/admin/rooms/[id]/route.ts
+++ book-it-app/app/api/admin/rooms/[id]/route.ts
@@ -1,26 +1,30 @@
 // @path: @/app/api/admin/rooms/[id]/route.ts
+// модулі та функції для взаємодії з базою даних та обробки запитів
 import dbConnect from "@/backend/config/dbConnect";
 import { deleteRoom, updateRoom } from "@/backend/controllers/roomControllers";
 import { createEdgeRouter } from "next-connect";
 import { NextRequest } from "next/server";
 
+// очікуваний формат об'єкта контексту, що містить параметр id
 interface RequestContext {
   params: {
-    id: string;
+    id:string;
   };
 }
-
+// маршрутизатор для обробки PUT та DELETE запитів з параметром id
 const router = createEdgeRouter<NextRequest, RequestContext>();
-
+// підключення до бази даних перед роботою з запитами
 dbConnect();
-
+// додаємо функції обробки запитів до маршрутизатора
 router.put(updateRoom);
 router.delete(deleteRoom);
 
-export async function PUT(request: NextRequest, ctx: RequestContext) {
+// викликає виконання маршрутизатора з вхідними параметрами
+export async function PUT(request:NextRequest, ctx:RequestContext) {
   return router.run(request, ctx);
 }
 
-export async function DELETE(request: NextRequest, ctx: RequestContext) {
+// викликає виконання маршрутизатора з вхідними параметрами
+export async function DELETE(request:NextRequest, ctx:RequestContext) {
   return router.run(request, ctx);
 }
