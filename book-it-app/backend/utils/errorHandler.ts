d2818e01921088d3697bebba97b1a9a210745859
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-12-01 21:11:08 +0200

    refactor: improve code organization
    update errorHandler.ts

--- book-it-app/backend/utils/errorHandler.ts
+++ book-it-app/backend/utils/errorHandler.ts
@@ -1,9 +1,13 @@
 // @path: @/backend/utils/errorHandler.ts
+// використовується для створення об'єктів помилок з певним статус-кодом
 class ErrorHandler extends Error {
-  statusCode: number;
-
-  constructor(errMessage: string, statusCode: number) {
+  // поле для збереження статус-коду помилки
+  statusCode:number;
+  // конструктор приймає повідомлення про помилку та статус-код
+  constructor(errMessage:string, statusCode:number) {
+    // виклик конструктора Error з повідомленням про помилку
     super(errMessage);
+    // ініціалізація коду для даної помилки
     this.statusCode = statusCode;
   }
 }
