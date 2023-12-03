76bcc3e626dcf0e5ed041385f5245f24efe17405
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-12-03 19:14:34 +0200

    chore: refactor code to maintain code style consistency
    update "@/app/page.tsx"

--- book-it-app/app/page.tsx
+++ book-it-app/app/page.tsx
@@ -1,6 +1,26 @@
 // @path: @/app/page.tsx
 import Home from "@/components/Home";
 
-export default function HomePage() {
+// export const dynamic = "force-dynamic";
+// функція-запит до сервера на отримання списку кімнат
+const getRooms = async () => {
+  // виконання запиту до сервера за допомогою fetch
+  const res = await fetch("http://localhost:3000/api/rooms", {
+    // опції запиту next.js
+    next: {
+      tags: ["Rooms"],
+    },
+  });
+  // повернення результату запиту у форматі JSON
+  return res.json();
+};
+
+// виконує запит до сервера та відображає компонент Home
+export default async function HomePage() {
+  // виклик функції для отримання списку кімнат
+  const rooms = await getRooms();
+  // вивід значення кількості кімнат на сторінку
+  console.log("resPerPage => ", rooms.resPerPage);
+  // повернення компонента
   return <Home />;
 }
