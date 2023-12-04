df559c5ca9ec45c158191df755648baff6dfed18
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-12-04 11:09:00 +0200

    docs: write description of certain functionality
    update "/app/GlobalProvider.tsx"

--- book-it-app/app/GlobalProvider.tsx
+++ book-it-app/app/GlobalProvider.tsx
@@ -1,10 +1,14 @@
 // @path: @/app/GlobalProvider.tsx
+// компонент для виведення повідомлень
 import { Toaster } from "react-hot-toast";
 
+// надає зручний спосіб включити глобальний Toaster для виведення toast-повідомлень
 export function GlobalProvider({ children }:{ children:React.ReactNode }) {
   return (
     <>
+      {/* забезпечує відображення повідомлень поверх усього іншого */}
       <Toaster />
+      {/* це контент, обгорнутий у GlobalProvider. */}
       {children}
     </>
   );
