98006c754efc89f9850473ad4527160c7ae3e870
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-12-04 16:59:10 +0200

    perf: make changes to improve overall project performance
    update next.config.js

--- book-it-app/next.config.js
+++ book-it-app/next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // змінні середовища для використання в клієнтській та серверній частині
   env: {
+    API_URL: "http://localhost:3000",
     DB_LOCAL_URI: "mongodb://127.0.0.1:27017/bookit",
     DB_URI: "",
+  },
+  // налаштування для оптимізованого завантаження зображень
+  images: {
+    // довірений домен для завантаження зображень
+    domains: ["res.cloudinary.com"],
   },
 };
 
