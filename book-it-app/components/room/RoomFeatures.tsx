37dab3de802c91aedbe28c1fc984e2a791273767
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-12-05 13:03:50 +0200

    feat: create new data visualization component
    create "@/components/room/RoomFeatures.tsx"

---
+++ book-it-app/components/room/RoomFeatures.tsx
@@ -0,0 +1,83 @@
+// @path: @/components/room/RoomFeatures.tsx
+import { IRoom } from "@/backend/models/room";
+import React from "react";
+// Інтерфейс для типів властивостей компонента
+interface Props {
+  room: IRoom;
+}
+
+// компонент для відображення особливостей кімнати
+const RoomFeatures = ({ room }: Props) => {
+  return (
+    <div className="features mt-5">
+      {/* заголовок для блоку особливостей */}
+      <h3 className="mb-4">Features:</h3>
+      {/* кожна особливість представлена окремим блоком */}
+      <div className="room-feature">
+        <i className="fa fa-cog fa-fw fa-users" aria-hidden="true"></i>
+        <p>{room?.guestCapacity} Guests</p>
+      </div>
+      <div className="room-feature">
+        <i className="fa fa-cog fa-fw fa-bed" aria-hidden="true"></i>
+        <p>{room?.numOfBeds} Beds</p>
+      </div>
+      <div className="room-feature">
+        <i
+          className={
+            room?.isBreakfast
+              ? "fa fa-check text-success"
+              : "fa fa-times text-danger"
+          }
+          aria-hidden="true"
+        ></i>
+        <p>Breakfast</p>
+      </div>
+      <div className="room-feature">
+        <i
+          className={
+            room?.isInternet
+              ? "fa fa-check text-success"
+              : "fa fa-times text-danger"
+          }
+          aria-hidden="true"
+        ></i>
+        <p>Internet</p>
+      </div>
+      <div className="room-feature">
+        <i
+          className={
+            room?.isAirConditioned
+              ? "fa fa-check text-success"
+              : "fa fa-times text-danger"
+          }
+          aria-hidden="true"
+        ></i>
+        <p>Air Conditioned</p>
+      </div>
+      <div className="room-feature">
+        <i
+          className={
+            room?.isPetsAllowed
+              ? "fa fa-check text-success"
+              : "fa fa-times text-danger"
+          }
+          aria-hidden="true"
+        ></i>
+        <p>Pets Allowed</p>
+      </div>
+      <div className="room-feature">
+        <i
+          className={
+            room?.isRoomCleaning
+              ? "fa fa-check text-success"
+              : "fa fa-times text-danger"
+          }
+          aria-hidden="true"
+        ></i>
+        <p>Room Cleaning</p>
+      </div>
+    </div>
+  );
+};
+
+export default RoomFeatures;
