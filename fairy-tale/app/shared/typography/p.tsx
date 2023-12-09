248db522b4284d7cfab09c91e7c14d3bf2c481cc
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-12-09 22:03:28 +0200

    init: add necessary files

---
+++ fairy-tale/app/shared/typography/p.tsx
@@ -0,0 +1,13 @@
+// @path: @/app/shared/typography/p.tsx
+import { HTMLAttributes } from 'react';
+import styles from './p.module.css';
+
+type PProps = WithChildren<HTMLAttributes<HTMLParagraphElement>>;
+
+export function P({ children, ...props }: PProps) {
+  return (
+    <p className={styles.root} {...props}>
+      {children}
+    </p>
+  );
+}
