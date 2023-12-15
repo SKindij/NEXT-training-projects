55bc63b1843d01e62c364b56ef9902d5bcd89c71
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-12-15 21:13:32 +0200

    feat: implement search functionality in app
    add /search/page.tsx

---
+++ book-it-app/app/search/page.tsx
@@ -0,0 +1,17 @@
+// @path: @/app/search/page.tsx
+import Search from "@/components/Search";
+import React from "react";
+
+export const metadata = {
+  title: "Search Rooms",
+};
+
+const SearchPage = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
+
+export default SearchPage;
