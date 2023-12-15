65c3cd6442905eff6f90811fc7130b8b5bc9c8dc
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-12-15 21:05:46 +0200

    feat: implement search functionality in app
    Update page.tsx

--- book-it-app/app/page.tsx
+++ book-it-app/app/page.tsx
@@ -7,24 +7,29 @@ export const metadata = {
 };
 
 // функція-запит до сервера на отримання списку кімнат
-const getRooms = async () => {
+const getRooms = async (searchParams: string) => {
+  const urlParams = new URLSearchParams(searchParams);
+  const queryString = urlParams.toString();
   // виконання запиту до сервера за допомогою fetch
-  const res = await fetch(`${process.env.API_URL}/api/rooms`);
+  const res = await fetch(`${process.env.API_URL}/api/rooms?${queryString}`, {
+    cache: "no-cache",
+  });
   // повернення результату запиту у форматі JSON
   return res.json();
 };
 
 // виконує запит до сервера та відображає компонент Home
-export default async function HomePage() {
+export default async function HomePage({searchParams}: {
+  searchParams: string;
+}) {
   // виклик функції для отримання списку кімнат
-  const data = await getRooms();
+  const data = await getRooms(searchParams);
 
   if (data?.message) {
     return <Error error={data} />;
   }
-  
   // вивід значення кількості кімнат на сторінку
-  console.log("resPerPage => ", rooms.resPerPage);
+  console.log("resPerPage => ", data.resPerPage);
   // повернення компонента
   return <Home data={data} />;
 }
