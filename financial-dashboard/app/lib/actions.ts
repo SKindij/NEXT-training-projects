b036141f88b063533e112c54764d8a6ec8a28477
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-12-31 09:53:54 +0200

    refactor: extract a reusable component from the existing code

--- financial-dashboard/app/lib/actions.ts
+++ financial-dashboard/app/lib/actions.ts
@@ -14,61 +14,33 @@ const FormSchema = z.object( {
   id: z.string(),
   customerId: z.string(),
   // забезпечує перетворення значення на число
-  amount: z.coerce.number()
-    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  amount: z.coerce.number(),
   // перевірка, що значення є одним із заданих переліком
-  status: z.enum(['pending', 'paid']), {
-    invalid_type_error: 'Please select an invoice status.',
-  }),
+  status: z.enum(['pending', 'paid']),
   date: z.string(),
 } );
 // визначення окремої схеми для створення рахунку, виключаючи id і date
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export type State = {
-  errors?: {
-    customerId?: string[];
-    amount?: string[];
-    status?: string[];
-  };
-  message?: string | null;
-};
-
 // додає дані щодо нового рахунку в базу даних
-export async function createInvoice(prevState:State, formData:FormData) {
+export async function createInvoice(formData:FormData) {
   // витягуємо необхідні дані з форми та валідуємо їх
-  const validatedFields = CreateInvoice.safeParse({
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  }); 
-  // перевіряємо наявність помилок при валідації
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Create Invoice.',
-    };
-  }
-  // витягуємо дані після валідації
-  const { customerId, amount, status } = validatedFields.data;
+  });
   // переводимо суму рахунку з доларів в центи
   const amountInCents = amount * 100;
   // отримуємо поточну дату у форматі ISO та відділяємо час від дати
   const date = new Date().toISOString().split('T')[0];
   // test it out:
   console.log(`amountInCents: ${amountInCents} on ${date}.`);
-  try {
-    // використовуємо sql-запит для вставки нового рахунку в базу даних
-    await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
-  } catch (error) {
-    return {
-      message: 'Database Error: Failed to Create Invoice.',
-    };
-  }
-  
+  // використовуємо sql-запит для вставки нового рахунку в базу даних
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -85,16 +57,12 @@ export async function updateInvoice(id:string, formData:FormData) {
   });
   // переводимо суму рахунку з доларів в центи
   const amountInCents = amount * 100;
-  try {
-    // використовуємо sql-запит для оновлення в базу даних
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
-  } catch (error) {
-    return { message: 'Database Error: Failed to Update Invoice.' };
-  }
+  // використовуємо sql-запит для оновлення в базу даних
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
   // для перевалідації шляху та перенаправлення на потрібну сторінку
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -102,12 +70,7 @@ export async function updateInvoice(id:string, formData:FormData) {
 
 
 export async function deleteInvoice(id:string) {
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    // ініціює новий запит на сервер і повторно відтворить таблицю
-    revalidatePath('/dashboard/invoices');
-	return { message: 'Deleted Invoice.' };
-  } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
-  }	
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  // ініціює новий запит на сервер і повторно відтворить таблицю
+  revalidatePath('/dashboard/invoices');
 }
